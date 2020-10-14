b9260f035b9319182a1bd7da94c2b6b1e68dfb34
Guilherme Henrique Lemes de Godoy <guilherme.hlg.eng@gmail.com>
2020-10-13 22:44:06 -0300

    feat: finaliza dia 2 NLW 3

---
+++ backend/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import OrphanagesController from './controllers/OrphanagesController'
+import './database/connection'
+import multer from 'multer'
+import uploadConfig from './config/upload'
+
+const routes = Router()
+const upload = multer(uploadConfig)
+
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
+routes.get('/orphanages', OrphanagesController.index)
+routes.get('/orphanages/:id', OrphanagesController.show)
+
+export default routes
